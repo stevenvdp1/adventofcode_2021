92db24568c7081718dabf42f38ed827fa2982b66
steven.vandepoel <stevenvandepoel@gmail.com>
2021-12-02 09:50:35 +0100

    day2

---
+++ day2/day2.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').split('\r\n');
+
+
+const answer1 = (data) => {
+    let x = 0
+    let y = 0
+    for (let line of data) {
+        const [direction, distance] = line.split(' ')
+        if (direction === 'forward') x += parseInt(distance)
+        else if (direction === 'up') y -= parseInt(distance)
+        else if (direction === 'down') y += parseInt(distance)
+    }
+    return x * y
+}
+
+
+const answer2 = (data) => {
+    let aim = 0
+    let x = 0
+    let y = 0
+
+    for (let line of data) {
+        const [direction, distance] = line.split(' ')
+        if (direction === 'forward') {
+            x += parseInt(distance)
+            y += (parseInt(distance) * aim)
+        }
+        else if (direction === 'up') aim -= parseInt(distance)
+        else if (direction === 'down') aim += parseInt(distance)
+    }
+    return x*y
+}
+
+console.log(answer1(data))
+console.log(answer2(data))
