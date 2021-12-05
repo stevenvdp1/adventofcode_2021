df296fff8d090e6941c3db503c39fea9bfccef79
steven.vandepoel <stevenvandepoel@gmail.com>
2021-12-05 15:42:36 +0100

    day4+5

---
+++ day4/day4.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const path = require('path');
+
+const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').split('\r\n');
+
+const readInput = (data) => {
+    let numbers = data.shift().split(',').map(Number);
+    data = data.map(line => line.trim().replace(/  /gi, ' ').split(' '));
+    data.shift();
+    let cards = []
+    let currentCard = []
+    for (let i = 0; i < data.length; i++) {
+        if (data[i][0] === '') {
+            cards.push(currentCard)
+            currentCard = []
+        }
+        else {
+            currentCard.push(data[i].map(Number))
+        }
+    }
+    cards.push(currentCard)
+
+    return [numbers, cards]
+}
+
+const findWinningCard = (data) => {
+    let [numbers, cards] = readInput(data)
+    let currentNumber
+    let bingoCard = null
+    while (numbers.length > 0 && !bingoCard) {
+        currentNumber = numbers.shift()
+        for (let card of cards) {
+            let rowIndex = card.findIndex(row => row.includes(currentNumber))
+            if (rowIndex !== -1) {
+                let colIndex = card[rowIndex].findIndex(col => col === currentNumber)
+                card[rowIndex][colIndex] = 'X'
+            }
+            if (doesCardHaveBingo(card)) {
+                bingoCard = card
+                break
+            }
+        }
+    }
+    return bingoCard.flat().filter(num => num !== 'X').reduce((acc, curr) => acc + curr, 0) * currentNumber
+}
+
+const doesCardHaveBingo = (card) => {
+    if (card.some(row => row.every(col => col === 'X'))) return true
+    for (let i = 0; i < card.length; i++) {
+        if (card.every(row => row[i] === 'X')) return true
+    }
+    return false
+}
+
+const findLosingCard = (data) => {
+    let [numbers, cards] = readInput(data)
+    let currentNumber
+    let losingCard = null
+    while (!losingCard) {
+        currentNumber = numbers.shift()
+        for (let card of cards) {
+            let rowIndex = card.findIndex(row => row.includes(currentNumber))
+            if (rowIndex !== -1) {
+                let colIndex = card[rowIndex].findIndex(col => col === currentNumber)
+                card[rowIndex][colIndex] = 'X'
+            }
+        }
+        if (cards.length > 1) cards = cards.filter(card => !doesCardHaveBingo(card))
+        else if (doesCardHaveBingo(cards[0])) losingCard = cards[0]
+    }
+    return cards.flat(2).filter(num => num !== 'X').reduce((acc, curr) => acc + curr, 0) * currentNumber
+}
+
+
+console.log('Answer1: ',findWinningCard([...data]));
+console.log('Answer2: ',findLosingCard([...data]));
---
+++ day5/day5.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const path = require('path');
+
+const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').split('\r\n').map(line => line.split(' -> ').map(point => point.split(',').map(Number)));
+
+const getDiagramNoDiagonal = (data) => {
+    const maxX = Math.max(...data.map(line => Math.max(line[0][0], line[1][0])))
+    const maxY = Math.max(...data.map(line => Math.max(line[0][1], line[1][1])))
+    let diagram = new Array(maxX + 1).fill(0).map(() => new Array(maxY + 1).fill(0))
+    data.forEach(line => {
+        let [[x_1, y_1], [x_2, y_2]] = line
+        if (x_1 === x_2) {
+            const[start, end] = [y_1, y_2].sort((a, b) => a - b)
+            for (let i = start; i <= end; i++){
+                diagram[x_1][i]++
+            }
+        }
+        else if (y_1 === y_2) {
+            const[start, end] = [x_1, x_2].sort((a, b) => a - b)
+            for (let i = start; i <= end; i++){
+                diagram[i][y_1]++
+            }
+        }
+    })
+    return diagram
+}
+
+const getDiagram = (data) => {
+    const maxX = Math.max(...data.map(line => Math.max(line[0][0], line[1][0])))
+    const maxY = Math.max(...data.map(line => Math.max(line[0][1], line[1][1])))
+    let diagram = new Array(maxX + 1).fill(0).map(() => new Array(maxY + 1).fill(0))
+    data.forEach(line => {
+        let [[x_1, y_1], [x_2, y_2]] = line
+        if (x_1 === x_2) {
+            const[start, end] = [y_1, y_2].sort((a, b) => a - b)
+            for (let i = start; i <= end; i++){
+                diagram[x_1][i]++
+            }
+        }
+        else if (y_1 === y_2) {
+            const[start, end] = [x_1, x_2].sort((a, b) => a - b)
+            for (let i = start; i <= end; i++){
+                diagram[i][y_1]++
+            }
+        }
+        else{
+            const length = Math.abs(x_1 - x_2)+1
+            let xDirection = x_1 < x_2 ? 1 : -1
+            let yDirection = y_1 < y_2 ? 1 : -1
+            for(let i = 0; i < length; i++){
+                diagram[x_1 + i * xDirection][y_1 + i * yDirection]++
+            }
+        }
+    })
+    return diagram
+}
+
+console.log('Answer1:', getDiagramNoDiagonal(data).flat().filter(x => x > 1).length)
+console.log('Answer2:', getDiagram(data).flat().filter(x => x > 1).length)
