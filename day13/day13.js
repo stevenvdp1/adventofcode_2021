c324fef9610704290af06b724405901c2ed578ed
steven.vandepoel <stevenvandepoel@gmail.com>
2021-12-13 14:59:03 +0100

    day13 refactoring

--- day13/day13.js
+++ day13/day13.js
@@ -2,56 +2,55 @@ console.time('totalTime')
 const fs = require('fs');
 const path = require('path');
 const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8').split('\r\n\r\n');
-const coordinates = data[0].split('\r\n').map(row => row.split(','));
+const coordinates = data[0].split('\r\n').map(row => row.split(',').map(coordinate => parseInt(coordinate)));
 const folds = data[1].split('\r\n').map(row => row.replace('fold along ', '').split('='));
 
-
-const getGrid = (folds, coordinates) => {
-    const height = folds.find(fold => fold[0] === 'x')[1]*2
-    const width = folds.find(fold => fold[0] === 'y')[1]*2
-    const grid = new Array(width + 1).fill(0).map(() => new Array(height + 1).fill(false));
-
-    coordinates.forEach(coordinate => {
-        grid[coordinate[1]][coordinate[0]] = true;
-    })
-    return grid
-}
-
-const doFold = (grid, fold) => {
-    let newGrid
-    if (fold[0] === 'x') {
-        newGrid = Array(grid.length).fill(0).map(() => new Array((grid[0].length - 1) / 2).fill(false));
-        for (let y = 0; y < grid.length; y++) {
-            for (let x = 0; x < (grid[0].length - 1) / 2; x++) {
-                newGrid[y][x] = Boolean(grid[y][x] + grid[y][grid[0].length - 1 - x]);
+const printTable = (coordinates) => {
+    const x_max = Math.max(...coordinates.map(coordinate => coordinate[0]));
+    const y_max = Math.max(...coordinates.map(coordinate => coordinate[1]));
+    for (let y = 0; y <= y_max; y++) {
+        for (let x = 0; x <= x_max; x++) {
+            if (coordinates.find(coordinate => coordinate[0] === x && coordinate[1] === y)) {
+                process.stdout.write('█');
+            } else {
+                process.stdout.write(' ');
             }
         }
+        process.stdout.write('\n');
     }
-    if (fold[0] === 'y') {
-        newGrid = Array((grid.length - 1) / 2).fill(0).map(() => new Array(grid[0].length).fill(false));
-        for (let y = 0; y < (grid.length - 1) / 2; y++) {
-            for (let x = 0; x < grid[0].length; x++) {
-                newGrid[y][x] = Boolean(grid[y][x] + grid[grid.length - 1 - y][x]);
-            }
+    process.stdout.write('\n');
+}
+
+const removeDuplicates = (coordinates) => {
+    return coordinates.reduce((acc, curr) => {
+        if (acc.some(a => a[0] === curr[0] && a[1] === curr[1])) return acc
+        return [...acc, curr]
+    }, [])
+}
+
+const doAllFolds = (coordinates, folds) => {
+    folds.forEach(fold => {
+        coordinates = doFold(coordinates, fold);
+    })
+    return coordinates
+}
+
+const doFold = (coordinates, fold) => {
+    const [axis, value] = fold;
+    let newCoordinates = coordinates.map(coordinate => {
+        if (axis === 'x') {
+            if (coordinate[0] < value) return [coordinate[0], coordinate[1]];
+            else if (coordinate[0] > value) return [Math.abs(coordinate[0] - (value * 2)), coordinate[1]];
         }
-    }
-    return newGrid
+        else if (axis === 'y') {
+            if (coordinate[1] < value) return [coordinate[0], coordinate[1]];
+            else if (coordinate[1] > value) return [coordinate[0], Math.abs(coordinate[1] - (value * 2))];
+        }
+    })
+    return removeDuplicates(newCoordinates)
 }
 
-const doAllFolds = (grid, folds) => {
-    folds.forEach((fold,index) => {
-        grid = doFold(grid, fold)
-    })
-
-    return grid.map(row => {
-        return row.map(cell => { 
-            if (cell) return'#'
-            return '.'
-        })
-    })
-}
-
-console.log('Answer1:', doFold(getGrid([...folds], [...coordinates]), folds[0]).flat().filter(Boolean).length)
-console.table(doAllFolds(getGrid(folds, coordinates), folds))
+console.log('Answer1:', doFold([...coordinates], folds[0]).length)
+printTable(doAllFolds(coordinates, folds));
 
 console.timeEnd('totalTime')
